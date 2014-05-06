5fbd4460c0cc0f9d9a55501689db3f17b14b40c4
Shiva Huang <shiva.huang@cocoazen.com>
2014-05-06 13:33:58 +0800

    Adapt for bootstrap_helper

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module EasyDoorplate
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    SITE_NAME = "台灣門牌查詢"
   end
 end
