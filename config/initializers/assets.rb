6738d9bb11e6977a8a8df0a278ec1b8425d34c98
Shiva Huang <shiva.huang@cocoazen.com>
2014-05-29 18:18:21 +0800

    Include twitter typeahead.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
 Rails.application.config.assets.precompile += %w( welcome.css )
 Rails.application.config.assets.precompile += %w( welcome.js )
 Rails.application.config.assets.precompile += %w( twitter/bootstrap/button.js )
+Rails.application.config.assets.precompile += %w( typeahead.bundle.min.js )
